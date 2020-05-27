1b7abb18e61e609b9dbb9f790d769eef1f07eb7f
Tom Dyson <tom@torchbox.com>
2020-05-28 00:29:13 +0100

    Roll back build dir creation in run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@
 python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
 
-# Make the build directory
-mkdir -p /tmp/build
 # Start Gunicorn processes
 exec gunicorn tomd.wsgi:application --bind 0.0.0.0:$PORT
