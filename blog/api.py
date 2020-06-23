4a3173d2eb43afe28e4c32ceccd7cbd2126e3e9c
Tom Dyson <tom@torchbox.com>
2020-06-23 15:12:37 +0100

    Allow API lookups by slug

--- blog/api.py
+++ blog/api.py
@@ -1,9 +1,34 @@
+from django.urls import path
+
 from wagtail.api.v2.views import PagesAPIViewSet
 from wagtail.api.v2.router import WagtailAPIRouter
-from wagtail.images.api.v2.views import ImagesAPIViewSet
+
+
+class CustomAPIEndpoint(PagesAPIViewSet):
+    """
+    Custom Pages API endpoint that allows finding pages by pk or slug
+    https://dev.to/wagtail/wagtail-api-how-to-customize-the-detail-url-2j3l
+    """
+
+    def detail_view(self, request, pk=None, slug=None):
+        param = pk
+        if slug is not None:
+            self.lookup_field = "slug"
+            param = slug
+        return super().detail_view(request, param)
+
+    @classmethod
+    def get_urlpatterns(cls):
+        """
+        This returns a list of URL patterns for the endpoint
+        """
+        return [
+            path("", cls.as_view({"get": "listing_view"}), name="listing"),
+            path("<int:pk>/", cls.as_view({"get": "detail_view"}), name="detail"),
+            path("<slug:slug>/", cls.as_view({"get": "detail_view"}), name="detail"),
+            path("find/", cls.as_view({"get": "find_view"}), name="find"),
+        ]
+
 
 api_router = WagtailAPIRouter("wagtailapi")
-
-api_router.register_endpoint("pages", PagesAPIViewSet)
-api_router.register_endpoint("images", ImagesAPIViewSet)
-
+api_router.register_endpoint("pages", CustomAPIEndpoint)
