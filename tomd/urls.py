98cadff63966e42fb19f569eea7c603839ebf6c7
Tom Dyson <tom@torchbox.com>
2020-11-27 18:44:12 +0000

    Simplify URLs

--- tomd/urls.py
+++ tomd/urls.py
@@ -6,7 +6,6 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-from search import views as search_views
 from blog.api import api_router
 from wagtailnetlify import urls as netlify_urls
 
@@ -14,16 +13,12 @@ urlpatterns = [
     url(r"^django-admin/", admin.site.urls),
     url(r"^admin/", include(wagtailadmin_urls)),
     url(r"^documents/", include(wagtaildocs_urls)),
-    url(r"^search/$", search_views.search, name="search"),
     url(r"^api/v2/", api_router.urls),
     url(r"^netlify/", include(netlify_urls)),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     url(r"", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    url(r'^pages/', include(wagtail_urls)),
 ]
 
 
